9f8b66c5fb93e9a99e0917032e0b0246b501a49c
Michel van der Hulst <mvanderhulst@xebia.com>
2017-07-14 11:01:15 +0200

    Add hiding and showing modal

--- src/App.elm
+++ src/App.elm
@@ -1,8 +1,10 @@
 port module App exposing (..)
 
 import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
-
+import Html.Attributes exposing (src, class)
+import Html.Events exposing (onClick)
+import Http exposing (Error)
+import Json.Decode exposing (Decoder, at, string)
 
 ---- MODEL ----
 
@@ -10,12 +12,13 @@ import Html.Attributes exposing (src)
 type alias Model =
     { message : String
     , logo : String
+    , showingModal: Bool
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+    ( { message = "Your Elm App is working!", logo = path, showingModal = True }, Cmd.none )
 
 
 
@@ -24,14 +27,29 @@ init path =
 
 type Msg
     = OpenDialog String
+    | CloseDialog
+    | ChooseColor
+    | CallApi (Result Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         (OpenDialog string) ->
-            ( { model | message = string }, Cmd.none )
+            ( { model | message = string, showingModal = True }, Cmd.none )
 
+        CloseDialog ->
+            ( { model | showingModal = False }, Cmd.none )
+
+        ChooseColor ->
+            ( model, chooseColor "lamp 1" )
+
+
+        CallApi (Ok json) ->
+            ( model, Cmd.none )
+
+        CallApi (Err _) ->
+            ( model, Cmd.none )
 
 
 ---- VIEW ----
@@ -39,12 +57,40 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
+  case model.showingModal of
+      True -> modal model
+
+      False -> noModal
+
+modal : Model -> Html Msg
+modal model =
+    div [ class "modal" ]
+        [ text "this is a modal"
+        ,  div [ onClick CloseDialog ] [text "close"]
         ]
 
+noModal : Html Msg
+noModal = div [] [ text "no modal" ]
 
+callForApi : Model -> Html Msg
+callForApi model =
+  div [onClick CallApi ] [ text "Call api" ]
+
+chooseColor : String -> Cmd Msg
+chooseColor lamp =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ lamp
+
+    request =
+      Http.get url decodeChooseColorResponse
+  in
+    Http.send CallApi request
+
+
+decodeChooseColorResponse : Decoder String
+decodeChooseColorResponse =
+  at ["data", "image_url"] string
 
 -- PORTS
 
