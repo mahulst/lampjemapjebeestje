e040684210af607fcaa79d9a083b53309e19acd6
Michel van der Hulst <michel@voorkanter.com>
2017-07-22 13:22:09 +0200

    Call actual api, prettify modal, include material design sliders

--- src/App.elm
+++ src/App.elm
@@ -1,24 +1,48 @@
 port module App exposing (..)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src, class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, text, div, img, input)
+import Html.Attributes as H exposing (src, class, type_)
+import Html.Events exposing (onClick, onInput)
 import Http exposing (Error)
 import Json.Decode exposing (Decoder, at, string)
+import Http
+import Json.Encode
+import Material.Slider as Slider
+import Material.Button as Button
+import Material.Options as Options
+import Material
+import Material.Icon as Icon
+
 
 ---- MODEL ----
 
 
 type alias Model =
     { message : String
-    , logo : String
-    , showingModal: Bool
+    , lamp : String
+    , showingModal : Modals
+    , color : Color
+    , mdl : Material.Model
     }
 
 
-init : String -> ( Model, Cmd Msg )
-init path =
-    ( { message = "Your Elm App is working!", logo = path, showingModal = True }, Cmd.none )
+type Modals
+    = ColorPicker
+    | Info
+    | FlameThrower
+    | None
+
+
+type alias Color =
+    { red : Float
+    , blue : Float
+    , green : Float
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { message = "", lamp = "", showingModal = None, color = { red = 50, blue = 50, green = 50 }, mdl = Material.model }, Cmd.none )
 
 
 
@@ -26,29 +50,71 @@ init path =
 
 
 type Msg
-    = OpenDialog String
+    = OpenColorPicker String
+    | OpenInfo
     | CloseDialog
     | ChooseColor
     | CallApi (Result Error String)
+    | UpdateRed Float
+    | UpdateBlue Float
+    | UpdateGreen Float
+    | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        (OpenDialog string) ->
-            ( { model | message = string, showingModal = True }, Cmd.none )
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
+        OpenColorPicker string ->
+            ( { model | lamp = string, showingModal = ColorPicker }, Cmd.none )
+
+        OpenInfo ->
+            ( { model | showingModal = Info }, Cmd.none )
 
         CloseDialog ->
-            ( { model | showingModal = False }, Cmd.none )
+            ( { model | showingModal = None, message = "" }, Cmd.none )
 
         ChooseColor ->
-            ( model, chooseColor "lamp 1" )
+            ( model, chooseColor "test" model.color )
 
         CallApi (Ok json) ->
-            ( model, Cmd.none )
+            ( { model | showingModal = None, message = "" }, Cmd.none )
 
         CallApi (Err _) ->
-            ( model, Cmd.none )
+            ( { model | message = "Could not claim this light, already claimed" }, Cmd.none )
+
+        UpdateRed val ->
+            let
+                color =
+                    model.color
+
+                newColor =
+                    { color | red = val }
+            in
+                ( { model | color = newColor }, Cmd.none )
+
+        UpdateBlue val ->
+            let
+                color =
+                    model.color
+
+                newColor =
+                    { color | blue = val }
+            in
+                ( { model | color = newColor }, Cmd.none )
+
+        UpdateGreen val ->
+            let
+                color =
+                    model.color
+
+                newColor =
+                    { color | green = val }
+            in
+                ( { model | color = newColor }, Cmd.none )
+
 
 
 ---- VIEW ----
@@ -56,41 +122,162 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  case model.showingModal of
-      True -> modal model
+    case model.showingModal of
+        ColorPicker ->
+            modal model (modalColorPicker model)
 
-      False -> noModal
+        Info ->
+            modal model modalInfo
 
-modal : Model -> Html Msg
-modal model =
-    div [ class "modal" ]
-        [ text "this is a modal"
-        , div [ onClick CloseDialog ] [text "close"]
-        , callForApi model
+        FlameThrower ->
+            div [] [ navbar ]
+
+        None ->
+            div [] [ navbar ]
+
+
+modal : Model -> Html Msg -> Html Msg
+modal model content =
+    div []
+        [ navbar
+        , div [ class "modal" ]
+            [ div [ class "header" ]
+                [ div [ onClick CloseDialog, class "close" ] [ Icon.i "close" ] ]
+            , content
+            ]
         ]
 
-noModal : Html Msg
-noModal = div [] [ text "no modal" ]
 
-callForApi : Model -> Html Msg
-callForApi model =
-  div [onClick ChooseColor ] [ text "Call api" ]
+modalInfo : Html Msg
+modalInfo =
+    div [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " ]
 
-chooseColor : String -> Cmd Msg
-chooseColor lamp =
-  let
-    url =
-      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ lamp
 
-    request =
-      Http.get url decodeChooseColorResponse
-  in
-    Http.send CallApi request
+modalColorPicker : Model -> Html Msg
+modalColorPicker model =
+    div [ class "content" ]
+        [ preview model.color
+        , slider model.color.red UpdateRed "Red"
+        , slider model.color.blue UpdateBlue "Blue"
+        , slider model.color.green UpdateGreen "Green"
+        , errorMessage model.message
+        , callApiButton model
+        ]
+
+
+errorMessage : String -> Html msg
+errorMessage errorMessage =
+    if errorMessage == "" then
+        div [] []
+    else
+        div [ class "error" ] [ text errorMessage ]
+
+
+navbar : Html Msg
+navbar =
+    div [ class "nav" ]
+        [ div [ class "title" ] [ text "ColourMySha" ]
+        , div [ class "fire" ] [ Icon.i "warning" ]
+        , div [ class "info", onClick OpenInfo ] [ Icon.i "info" ]
+        ]
+
+
+callApiButton : Model -> Html Msg
+callApiButton model =
+    Button.render Mdl
+        [ 0 ]
+        model.mdl
+        [ Button.raised
+        , Button.ripple
+        , Options.onClick ChooseColor
+        ]
+        [ text "Set color" ]
 
 
 decodeChooseColorResponse : Decoder String
 decodeChooseColorResponse =
-  at ["data", "image_url"] string
+    at [ "data", "image_url" ] string
+
+
+preview : Color -> Html msg
+preview color =
+    let
+        red =
+            toString <| round <| (color.red / 100 * 256)
+
+        blue =
+            toString <| round <| (color.blue / 100 * 256)
+
+        green =
+            toString <| round <| (color.green / 100 * 256)
+
+        colorString =
+            "rgb(" ++ red ++ ", " ++ green ++ ", " ++ blue ++ ")"
+
+        style =
+            H.style
+                [ ( "background-color", colorString )
+                ]
+    in
+        div
+            [ class "color-preview", style ]
+            []
+
+
+
+--        div [ class "color-preview", style ] []
+
+
+slider : Float -> (Float -> Msg) -> String -> Html Msg
+slider float msg title =
+    div []
+        [ text title
+        , Slider.view
+            [ Slider.onChange msg
+            , Slider.value float
+            ]
+        ]
+
+
+
+-- ENCODERS
+
+
+colorEncoder : Color -> Json.Encode.Value
+colorEncoder color =
+    let
+        attributes =
+            [ ( "red", Json.Encode.float (color.red / 100) )
+            , ( "blue", Json.Encode.float (color.blue / 100) )
+            , ( "green", Json.Encode.float (color.green / 100) )
+            ]
+    in
+        Json.Encode.object attributes
+
+
+chooseColor : String -> Color -> Cmd Msg
+chooseColor lamp color =
+    let
+        url =
+            "http://kubernetes.strocamp.net:8888/zones/" ++ lamp ++ "/claim"
+
+        headers =
+            [ (Http.header "X-Auth-ApiKey" "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDb2xvck15U0hBMjAxNyIsInN1YiI6IktleUZvclNsYWNrRGlzdHJpYnV0aW9uIiwibmJmIjoxNDk5NjE4ODU2LCJleHAiOjE1MzExNTQ4NTYsImlhdCI6MTQ5OTYxODg1NiwianRpIjoiaWQ0MiIsInR5cCI6Imh0dHBzOi8va3ViZXJuZXRlcy5zdHJvY2FtcC5uZXQifQ.KoJy4eYMt5o9opSRlXmnTOx0jyBVGhq1ksUMs5-PRsqug3Fp60tctZnavHQDxTeBe-xwGfV_4G_gc3uXUKCqdD58FMVa6KoAB4kP-Fn1i-93RNgKYONceZo_rplreLa3T7wAjtFWnlPG0RC4etfRjSU904LJFfH2yCGeXDKDmukZ07w98VxMYWlNVWZdK7C55N4eUwSh_eo89wgISxJpI9lNOg89ZCEXTcTBcwnlQRqwSObLZ1r8OVfTcKSfMxXX5sFD6RlP06c7TtsDjBMxQ9_fcdfh6d7l4VqMuMwUrENrmsNc8iO7ZnHfNQoIjiBQsqE5CHSxisV0AP6DoR_R9Uq91TiQCxstdJ2rgZOIPJO8IlFP7Xw5eOdA1pJlry9pUBftMdMhsDHimzcPrVJRHzYmwQ38NLSWs9uOf1DGX8iF8HeGrzw3oDEFJyqaeb1sEOAxBrPI9YCshpCxqse-sfN7WRtAb9zCqJSBe1R-hs0sxhexgjSS1s9GTD2Hp6icbcEFNV_8m8KbYVgX_B6yIAiDXyJadSH7cc60T40Ji34LBvCai_kts37jSNnWVa-FkTAxpN5_PqVTTmPYlmPPltAA-u4x78KVZDAiqlhSvB75LgS0Hxrn73QAlYcI4Gp5kXHO_PNjw1Ocv73LSKzg6ObbMMqnrCz-YIIBUKAY17s") ]
+
+        request =
+            Http.request
+                { method = "PUT"
+                , url = url
+                , headers = headers
+                , body = colorEncoder color |> Http.jsonBody
+                , timeout = Nothing
+                , expect = Http.expectJson decodeChooseColorResponse
+                , withCredentials = False
+                }
+    in
+        Http.send CallApi request
+
+
 
 -- PORTS
 
@@ -98,21 +285,22 @@ decodeChooseColorResponse =
 port openDialog : (String -> msg) -> Sub msg
 
 
+
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    openDialog OpenDialog
+    openDialog OpenColorPicker
 
 
 
 ---- PROGRAM ----
 
 
-main : Program String Model Msg
+main : Program Never Model Msg
 main =
-    Html.programWithFlags
+    Html.program
         { view = view
         , init = init
         , update = update
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import './main.css';
 
-import logoPath from './logo.svg';
-const { App } = require('./App.elm');
+import { App } from './App.elm';
 
-var app = App.embed(document.getElementById('root'), logoPath);
+var app = App.embed(document.getElementById('root'));
 
 var ol = require('openlayers');
 var map, base_layers, overlay_layers;
 
 
-createMap({"layers": [{"path": "base", "name": "Base", "visible": true}, {"path": "trees", "name": "Trees", "visible": true}], "zoom_range": [7, 20], "extents": [52.292, 5.5405, 52.275, 5.5129]})
+createMap();
 
 
-function createMap(config) {
+function createMap() {
 
     base_layers = new ol.layer.Group({
         title: 'Base Layers',
@@ -28,405 +27,25 @@ function createMap(config) {
     map = new ol.Map({
         layers: [base_layers, overlay_layers],
         target: 'map',
-        controls: ol.control.defaults({
-            attributionOptions: ({
-                collapsible: false
-            })
-        }).extend([new ol.control.ScaleLine()]),
+        controls: [],
         view: new ol.View({
             center: (new ol.geom.Point([5.52579, 52.28488])).transform('EPSG:4326', 'EPSG:3857').getCoordinates(),
             zoom: 17,
         })
     });
 
-    // var layerSwitcher = new ol.control.LayerSwitcher();
-    // map.addControl(layerSwitcher);
-
-    addVectorLayers({
-        "layers": [
-            {
-                "source": "Grid.json",
-                "z-index": 0,
-                "name": "Grid",
-                "visible": false
-            },
-            {
-                "source": "Tents and buildings.json",
-                "z-index": 2,
-                "name": "Tents and buildings",
-                "visible": true
-            },
-            {
-                "source": "Field names.json",
-                "z-index": 2,
-                "name": "Field names",
-                "visible": true
-            },
-            {
-                "source": "Campers.json",
-                "z-index": 2,
-                "name": "Campers",
-                "visible": true
-            },
-            {
-                "source": "Available from early buildup.json",
-                "z-index": 1,
-                "name": "Available from early buildup",
-                "visible": false
-            },
-            {
-                "source": "Emergency services route.json",
-                "z-index": 2,
-                "name": "Emergency services route",
-                "visible": false
-            },
-            {
-                "source": "Evacuation route.json",
-                "z-index": 2,
-                "name": "Evacuation route",
-                "visible": false
-            },
-            {
-                "source": "Team:Logistics.json",
-                "z-index": 2,
-                "name": "Team:Logistics",
-                "visible": false
-            },
-            {
-                "source": "Team:NOC.json",
-                "z-index": 3,
-                "name": "Team:NOC",
-                "visible": false
-            },
-            {
-                "source": "Power -- APPROXIMATE.json",
-                "z-index": 2,
-                "name": "Power -- APPROXIMATE",
-                "visible": false
-            },
-            {
-                "source": "Water -- APPROXIMATE.json",
-                "z-index": 2,
-                "name": "Water -- APPROXIMATE",
-                "visible": false
-            },
-            {
-                "source": "Sewage -- APPROXIMATE.json",
-                "z-index": 2,
-                "name": "Sewage -- APPROXIMATE",
-                "visible": false
-            }
-        ],
-        "styles": {
-            "Team:NOC": {
-                "team-noc ... labels": {
-                    "text-color": "blue",
-                    "selectable": false,
-                    "z-index": 4
-                },
-                "team-noc ... datenklos ... dk-and-label": {
-                    "text-selectable": false,
-                    "line-color": "cyan",
-                    "text-color": "blue",
-                    "polygon-fill": "cyan",
-                    "z-index": 4,
-                    "line-width": 1
-                },
-                "team-noc ... cables ... copper": {
-                    "line-color": "black",
-                    "z-index": 3,
-                    "line-width": 3
-                },
-                "team-noc ... cables ... fibre-single-mode": {
-                    "line-color": "green",
-                    "z-index": 3,
-                    "line-width": 3
-                },
-                "team-noc ... switch-in-tent": {
-                    "polygon-fill": "magenta",
-                    "line-color": "magenta",
-                    "z-index": 4,
-                    "line-width": 1
-                }
-            },
-            "Power -- APPROXIMATE": {
-                "infrastructure ... sketch ... power ... aluminium_4x120": {
-                    "line-color": "red",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... power ... aluminium_4x050": {
-                    "line-color": "red",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... power ... aluminium_4x095": {
-                    "line-color": "red",
-                    "line-width": 3
-                },
-                "objects ... power terminal": {
-                    "polygon-fill": "orange",
-                    "line-color": "black",
-                    "z-index": 5,
-                    "line-width": 5
-                },
-                "infrastructure ... sketch ... power ... aluminium_4x070": {
-                    "line-color": "red",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... power ... aluminium_4x150": {
-                    "line-color": "red",
-                    "line-width": 3
-                }
-            },
-            "Campers": {
-                "terrain ... camper spot": {
-                    "line-color": "#800080",
-                    "z-index": 0,
-                    "line-width": 2
-                }
-            },
-            "Team:Logistics": {
-                "team-logistics ... sketch": {
-                    "text-color": "black",
-                    "line-color": "black",
-                    "selectable": false,
-                    "line-width": 2
-                },
-                "team-logistics ... trash ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                },
-                "team-logistics ... trash": {
-                    "line-color": "black",
-                    "line-width": 2
-                }
-            },
-            "Available from early buildup": {
-                "meta ... restrictions ... early buildup": {
-                    "line-color": "yellow",
-                    "z-index": 0,
-                    "line-width": 3
-                }
-            },
-            "Emergency services route": {
-                "routes ... R03_calamiteitenroute": {
-                    "line-color": "red",
-                    "selectable": false,
-                    "line-width": 5
-                }
-            },
-            "Water -- APPROXIMATE": {
-                "infrastructure ... sketch ... water ... pe090": {
-                    "line-color": "blue",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... water ... pe080": {
-                    "line-color": "blue",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... water ... pe110": {
-                    "line-color": "blue",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... water ... pe040": {
-                    "line-color": "blue",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... water ... pe063": {
-                    "line-color": "blue",
-                    "line-width": 3
-                }
-            },
-            "Tents and buildings": {
-                "tents ... tarps": {
-                    "line-color": "blue",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... temporary toilet": {
-                    "line-color": "red",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... radio tower": {
-                    "line-color": "#c06000",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... portacabin": {
-                    "line-color": "#000000",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... gate": {
-                    "line-color": "#c06000",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... portacabin ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                },
-                "tents ... tents": {
-                    "line-color": "magenta",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... utility": {
-                    "line-color": "red",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "team-power ... generator ... tank": {
-                    "line-color": "orange",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                },
-                "tents ... tents ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                },
-                "team-power ... generator ... generator": {
-                    "line-color": "orange",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... toilet": {
-                    "line-color": "red",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "tents ... tarps ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                },
-                "objects ... buildings": {
-                    "line-color": "red",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "objects ... buildings ... container": {
-                    "line-color": "#000000",
-                    "z-index": 3,
-                    "line-width": 2
-                },
-                "team-power ... generator ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                },
-                "objects ... buildings ... container ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                }
-            },
-            "Grid": {
-                "meta ... grid ... legend": {
-                    "text-color": "black",
-                    "line-color": "black",
-                    "selectable": false,
-                    "line-width": 1
-                },
-                "meta ... grid": {
-                    "text-color": "black",
-                    "line-color": "black",
-                    "selectable": false,
-                    "line-width": 1
-                }
-            },
-            "Field names": {
-                "terrain ... fields ... labels": {
-                    "text-color": "black",
-                    "selectable": false
-                }
-            },
-            "Sewage -- APPROXIMATE": {
-                "infrastructure ... sketch ... sewage ... pers_pe090mm": {
-                    "line-color": "lightbrown",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... sewage ... pers_pe160mm": {
-                    "line-color": "lightbrown",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... sewage ... vrijverval_pvc315": {
-                    "line-color": "brown",
-                    "line-width": 3
-                },
-                "objects ... sewer hole": {
-                    "polygon-fill": "brown",
-                    "line-color": "brown",
-                    "z-index": 5,
-                    "line-width": 5
-                },
-                "infrastructure ... sketch ... sewage ... vrijverval_pvc250": {
-                    "line-color": "brown",
-                    "line-width": 3
-                },
-                "infrastructure ... sketch ... sewage ... vrijverval_pvc200": {
-                    "line-color": "brown",
-                    "line-width": 3
-                }
-            },
-            "Evacuation route": {
-                "safety ... evacuation point": {
-                    "line-color": "darkgreen",
-                    "selectable": false,
-                    "line-width": 1
-                },
-                "safety ... escape route": {
-                    "line-color": "darkgreen",
-                    "selectable": false,
-                    "line-width": 3
-                },
-                "safety ... escape route ... arrow lines": {
-                    "polygon-fill": "darkgreen",
-                    "selectable": false
-                }
-            }
-        }
+    map.on('singleclick', function (evt) {
+      var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+      openDialog();
     });
 
 
-    // The <canvas> element doesn't seem to get sized correctly
-    // on page load, which causes vector element hover to break.
-    // Update the size after a small delay.
+    function openDialog() {
+      app.ports.openDialog.send("test");
+    }
+
+
     setTimeout(function () {
         map.updateSize();
     }, 50);
-}
-
-function addVectorLayers(layer_data) {
-    layer_data.layers.map(function (index, layer) {
-        var vectorSource = new ol.source.Vector({
-            url: 'vector/' + layer.source,
-            format: new ol.format.GeoJSON()
-        });
-
-        var vectorLayer = new ol.layer.Vector({
-            title: layer.name,
-            source: vectorSource,
-            visible: layer.visible,
-            updateWhileAnimating: true,
-            updateWhileInteracting: true
-        });
-        vectorLayer.setZIndex(layer['z-index']);
-        overlay_layers.getLayers().push(vectorLayer);
-    });
-
-}
-
-
-
-
-map.on('singleclick', function (evt) {
-    var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-    console.log(lonlat);
-    openDialog();
-});
-
-function openDialog() {
-    app.ports.openDialog.send("Hello, world!");
 }
